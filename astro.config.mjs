71ce517a40e60d3ecf85f98701662e4584c7d523
lzw1622627989 <1622627989@qq.com>
2024-10-14 09:12:37 +0800

    提交文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import preact from '@astrojs/preact';
+
+import vue from '@astrojs/vue';
+import pagefind from 'astro-pagefind';
+
 // https://astro.build/config
 export default defineConfig( {
   site: "https://example.com",
-  integrations: [tailwind()]
+  integrations: [ tailwind(), preact(), vue(),pagefind() ]
 });
---
+++ src/components/Greeting.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>{{ greeting }}! Thank you for visiting!</h3>
+        <button @click=" randomMessage ">
+            New Greeting
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from 'vue';
+    interface propsType {
+        messages: string[];
+    }
+    const props = defineProps<propsType>();
+    console.log( props );
+    const greeting = ref(props.messages[ 0 ]);
+    function randomMessage () {
+        greeting.value = props.messages[ ( Math.floor( Math.random() * props.messages.length ) ) ]
+    }
+
+
+</script>
+
+<style lang="scss" scoped></style>
